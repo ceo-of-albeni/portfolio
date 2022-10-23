90c4cb85252e5a6414d2023095ff0a3257fcbc4c
Aliia <101452567+ceo-of-albeni@users.noreply.github.com>
2022-10-23 12:05:32 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 let btn = document.querySelector("button");
-let btn_div = document.querySelector("#button_div");
 let btn_list = document.querySelector("#button_list");
 
 function onClick() {
   if (btn_list.innerText.length < 164) {
     btn_list.innerText += " meow";
   } else {
-    return (btn_list.innerText = "");
+    return (btn_list.innerText = "kill me pls");
   }
 }
 
